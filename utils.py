da30f0c3a1a48b808a08ddb603468fd8d909641a
jonathan <jonathan.juayong@gmail.com>
2025-12-28 12:31:59 +1000

    update code to follow code standards

--- utils.py
+++ utils.py
@@ -3,12 +3,12 @@ from socket import AddressFamily
 
 import psutil
 
-mock_mac_address = "34-5A-60-22-18-B2"
+mockMacAddress = "34-5A-60-22-18-B2"
 
 
 class MockSnicaddr:
     def __init__(self,
-                 address=mock_mac_address,
+                 address=mockMacAddress,
                  netmask="255.255.255.0",
                  ptp=None,
                  broadcast=None,
@@ -23,7 +23,7 @@ class MockSnicaddr:
 class MockSConn:
     def __init__(self, port, status="NONE"):
         self.status = status
-        self.laddr = type('', (), {'port': port})()
+        self.laddr = type("", (), {"port": port})()
 
 
 def mockSConns():
@@ -37,13 +37,13 @@ def mockSConns():
     ]
 
 
-def mock_data(mac='34-5A-60-22-18-B2'):
+def mockData(mac="34-5A-60-22-18-B2"):
     return {
-        'active_ports': '7865, 6188, 63903, 139, 5040, 445, 49664, 5563, 32683, 49665, 63342, 135, 41017, 49677, 7680, 5990, 59330, 49668, 5146, 33683, 6189, 1462, 6341, 63344, 19294, 9080, 49669, 63812, 64411, 26822, 54936, 63343, 49670, 49690',
-        'computer_name': 'MSI', 'internet_speed': 'download: 82.44 Mb/s, upload: 28.00 Mb/s',
-        'ip_address': '192.168.1.102',
-        'mac_address': mac, 'operating_system': 'Windows',
-        'processor_model': 'Intel(R) Core(TM) i7-14650HX', 'system_time': '19:01:19'
+        "active_ports": "7865, 6188, 63903, 139, 5040, 445, 49664, 5563, 32683, 49665, 63342, 135, 41017, 49677, 7680, 5990, 59330, 49668, 5146, 33683, 6189, 1462, 6341, 63344, 19294, 9080, 49669, 63812, 64411, 26822, 54936, 63343, 49670, 49690",
+        "computer_name": "MSI", "internet_speed": "download: 82.44 Mb/s, upload: 28.00 Mb/s",
+        "ip_address": "192.168.1.102",
+        "mac_address": mac, "operating_system": "Windows",
+        "processor_model": "Intel(R) Core(TM) i7-14650HX", "system_time": "19:01:19"
     }
 
 
@@ -62,5 +62,5 @@ def mockNicAddresses(int1="Ethernet", int2="Bluetooth Network Connection"):
     }
 
 
-def temp_file(name):
+def tempFile(name):
     return f"{tempfile.gettempdir()}\\{name}.csv"
