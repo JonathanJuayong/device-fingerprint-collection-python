df603675d30bb119926726d8bb5bd9708517a783
jonathan <jonathan.juayong@gmail.com>
2025-12-28 12:35:13 +1000

    create tests for project

---
+++ tests/test_main.py
@@ -0,0 +1,102 @@
+import csv
+import os
+import pathlib
+import unittest
+from unittest.mock import patch, MagicMock
+
+from exeptions import DataCollectionException, UnsupportedOperatingSystemException
+from main import getMacAddress, getProcessorModel, getActivePorts, collectData, writeToCSV
+from utils import mockNicAddresses, mockMacAddress, mockSConns, mockData, tempFile
+
+
+class ProjectTest(unittest.TestCase):
+    def testGetMacAddressFetchesMacAddress(self):
+        addresses = mockNicAddresses()
+
+        testMockAddress = getMacAddress(addresses)
+        self.assertEqual(mockMacAddress, testMockAddress)
+
+    def testGetMacAddressRaisesExceptionIfNoMacAddressFound(self):
+        addresses = mockNicAddresses(int1="invalid", int2="invalid")
+        with self.assertRaises(DataCollectionException):
+            getMacAddress(addresses)
+
+    def testGetProcessorModelRaisesExceptionIfInvalidOperatingSystem(self):
+        with self.assertRaises(UnsupportedOperatingSystemException):
+            getProcessorModel("Invalid")
+
+    def testGetActivePortsReturnsActivePorts(self):
+        activePorts = {"443", "5173"}
+        testActivePorts = getActivePorts(mockSConns())
+        testActivePortsSet = set(testActivePorts.split(", "))
+        self.assertEqual(activePorts, testActivePortsSet)
+
+    @patch("main.speedtest.Speedtest")
+    def testCollectDataCreatesDictionaryWithExpectedKeys(self, mockSpeedtest):
+        # mock speedtest call
+        mockSpeedtestObject = MagicMock()
+        mockSpeedtestObject.download.return_value = 10000000
+        mockSpeedtestObject.upload.return_value = 10000000
+        mockSpeedtest.return_value = mockSpeedtestObject
+        deviceInfo = collectData()
+        deviceInfoKeys = deviceInfo.keys()
+
+        expectedKeys = [
+            "computer_name",
+            "operating_system",
+            "mac_address",
+            "processor_model",
+            "ip_address",
+            "internet_speed",
+            "active_ports",
+            "system_time"
+        ]
+
+        for key in expectedKeys:
+            self.assertIn(key, deviceInfoKeys)
+
+    def testWriteToCSVCreatesCSV(self):
+        filePath = tempFile("test_file1")
+        writeToCSV(mockData(), filePath)
+
+        self.assertTrue(pathlib.Path(filePath).exists())
+
+        os.remove(filePath)
+
+    def testWriteToCSVWritesDataToCSV(self):
+        filePath = tempFile("test_file2")
+        writeToCSV(mockData(), filePath)
+        expectedKeys = mockData().keys()
+
+        file = pathlib.Path(filePath)
+
+        with file.open(mode="r") as csvFile:
+            csvReader = csv.DictReader(csvFile)
+            for row in csvReader:
+                for key in expectedKeys:
+                    expectedValue = mockData()[key]
+                    actualValue = row[key]
+                    print(expectedValue, actualValue)
+                    self.assertEqual(expectedValue, actualValue)
+
+        os.remove(filePath)
+
+    def testWriteToCSVShouldNotDuplicateRecord(self):
+        filePath = tempFile("test_file3")
+
+        writeToCSV(mockData(), filePath)
+        writeToCSV(mockData(), filePath)
+        writeToCSV(mockData(), filePath)
+
+        file = pathlib.Path(filePath)
+
+        with file.open(mode="r") as csvFile:
+            csvReader = csv.DictReader(csvFile)
+            rowCount = list(csvReader)
+            self.assertEqual(1, len(rowCount))
+
+        os.remove(filePath)
+
+
+if __name__ == "__main__":
+    unittest.main()
